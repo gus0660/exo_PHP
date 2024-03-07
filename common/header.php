9539290d1433fa60f286aaf607bd813d4066fdc1
gus0660 <guillaumeroch3@gmail.com>
2024-03-07 09:13:00 +0100

    commentaire dans header.php

--- common/header.php
+++ common/header.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <!-- <meta name="description" content=""> ne pas oublier de positionner pour SEO -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php require "header.php"; ?>
 
+<?php require "footer.php"; ?>
     
 </body>
 </html>
