b29e1b2e085398a71a8b75315744e620d9e75ef4
gus0660 <guillaumeroch3@gmail.com>
2024-03-07 14:25:48 +0100

    codage index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,19 @@
-<?php require "header.php"; ?>
-
-<?php require "footer.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Presentation PHP-algo</h1>
+    <p>Création d'un petit générateur de mot de passe crypté</p>
+    <a href="keygenGenerator"KeyGenGenerator</a>
     
 </body>
 </html>
+
+
+<!-- <?php require "header.php"; ?>
+
+<?php require "footer.php"; ?> -->
