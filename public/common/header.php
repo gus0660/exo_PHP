ba5b5aaac31f1633b889b019a5a9546cd9d500fc
gus0660 <groch1@hotmail.fr>
2024-03-07 19:21:02 +0100

    début codage exo1.php

--- public/common/header.php
+++ public/common/header.php
@@ -10,3 +10,21 @@
     <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
 </head>
 <body>
+<main class="text-center">
+    <h1>Presentation PHP-algo</h1>
+    <p>Création d'un petit générateur de mot de passe crypté</p>
+    <a href="keygenGenerator">KeyGenGenerator</a>
+    <div><img src="images/logo.png" width="150px" alt="logo PHP"></div>
+    <div class="text-center">
+        <a href="public/pages/exo1.php">Exercice 1</a>
+        <a href="public/pages/exo2.php">Exercice 2</a>
+        <a href="public/pages/exo3.php">Exercice 3</a>
+        <a href="public/pages/exo4.php">Exercice 4</a>
+        <a href="public/pages/exo5.php">Exercice 5</a>
+        <a href="public/pages/exo6.php">Exercice 6</a>
+        <a href="public/pages/exo7.php">Exercice 7</a>
+        <a href="public/pages/exo8.php">Exercice 8</a>
+        <a href="public/pages/exo9.php">Exercice 9</a>
+        <a href="public/pages/exo10.php">Exercice 10</a>
+    </div>
+</main>
--- public/index.php
+++ public/index.php
@@ -1,30 +1,14 @@
-<main class="text-center">
-    <h1>Presentation PHP-algo</h1>
-    <p>Création d'un petit générateur de mot de passe crypté</p>
-    <a href="keygenGenerator">KeyGenGenerator</a>
-    <div><img src="images/logo.png" width="150px" alt="logo PHP"></div>
-    <div class="text-center">
-        <a href="public/pages/exo1.php">Exercice 1</a>
-        <a href="public/pages/exo2.php">Exercice 2</a>
-        <a href="public/pages/exo3.php">Exercice 3</a>
-        <a href="public/pages/exo4.php">Exercice 4</a>
-        <a href="public/pages/exo5.php">Exercice 5</a>
-        <a href="public/pages/exo6.php">Exercice 6</a>
-        <a href="public/pages/exo7.php">Exercice 7</a>
-        <a href="public/pages/exo8.php">Exercice 8</a>
-        <a href="public/pages/exo9.php">Exercice 9</a>
-        <a href="public/pages/exo10.php">Exercice 10</a>
-    </div>
-
-</main>
 
 
 
-<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+
 </body>
 
 </html>
 
 <?php
 include("common/header.php");
+include("common/footer.php");
+include("pages/exo1.php");
 ?>
