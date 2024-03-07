47ef6f4987fe8672f3388560bb71556c4f48ce67
gus0660 <guillaumeroch3@gmail.com>
2024-03-07 09:22:41 +0100

    logo dans images et link css

--- common/header.php
+++ common/header.php
@@ -5,5 +5,6 @@
      <meta name="description" content=""> <!--ne pas oublier de positionner pour SEO -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercisse PHP</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
