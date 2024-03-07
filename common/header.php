6a2b14318809eb74a17141854dfece2811b610a7
gus0660 <guillaumeroch3@gmail.com>
2024-03-07 09:13:56 +0100

    title dans header.php

--- common/header.php
+++ common/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <!-- <meta name="description" content=""> ne pas oublier de positionner pour SEO -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exercisse PHP</title>
 </head>
 <body>
