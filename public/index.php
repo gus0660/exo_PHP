be76b1b9bda385226460608e084833d4ce511745
gus0660 <guillaumeroch3@gmail.com>
2024-03-07 16:33:43 +0100

    imange logo php

--- public/index.php
+++ public/index.php
@@ -1,14 +1,19 @@
 
-<?php
-include("common/header.php");
-?>
-
-    <h1 class="">Presentation PHP-algo</h1>
+<main class="text-center">
+    <h1>Presentation PHP-algo</h1>
     <p>Création d'un petit générateur de mot de passe crypté</p>
-    <a href="keygenGenerator" >KeyGenGenerator</a>
+    <a href="keygenGenerator">KeyGenGenerator</a>
+    <div><img src="images/logo.png" width="150px" alt="logo PHP"></div>
+        
+        
     
+    
+</main>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
 
-
-
+<?php
+include("common/header.php");
+?>
