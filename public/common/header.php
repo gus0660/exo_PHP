7169170753768400d69bdd1716631fe72b8a7a3a
gus0660 <groch1@hotmail.fr>
2024-03-08 15:04:30 +0100

    color black sur a href

--- public/common/header.php
+++ public/common/header.php
@@ -19,7 +19,7 @@ include_once 'config.php';
     <a href="keygenGenerator">KeyGenGenerator</a>
     <div><img src="<?php echo BASE_URL; ?>images/logo.png" width="150px" alt="logo PHP"></div>
     <div class="text-center">
-        <a class="p-2"  href="index">Accueil</a>
+        <a class="p-2"  href="index" style="color: black;">Accueil</a>
         <a class="p-2"  href="exo1">Exercice 1</a>
         <a class="p-2"  href="exo2">Exercice 2</a>
         <a class="p-2"  href="exo3">Exercice 3</a>
