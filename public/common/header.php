5927eb446a6d3806774a024e876ccfccdd135bcf
gus0660 <groch1@hotmail.fr>
2024-03-08 15:08:57 +0100

    déplacement nodes module et images dans public

--- public/common/header.php
+++ public/common/header.php
@@ -9,8 +9,8 @@ include_once 'config.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercice PHP</title>
     <link rel="stylesheet" href="main">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>node_modules/bootstrap/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
 </head>
 <body>
 <main class="text-center">
