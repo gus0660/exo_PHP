25cdaee23c5cb19edcfcf66a5b1d15dce51dfb71
gus0660 <guillaumeroch3@gmail.com>
2024-03-08 09:16:46 +0100

    exo1 fait

--- public/pages/exo1.php
+++ public/pages/exo1.php
@@ -4,9 +4,21 @@ include("../common/footer.php");
 ?>
 <main class="text-center">
     <h2 class="p-4">EXERCICE 1</h2>
+    <form method="post">
+        <label for="table">Quelle table de multiplication voulez-vous afficher ?</label>
+        <input type="number" id="table" name="table" required>
+        <input type="submit" value="Afficher">
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $input = $_POST['table']; // Récupération de la valeur saisie par l'utilisateur
 
+        for ($result = 1; $result <= 10; $result++) {
+            $calc = $input * $result;
+            echo "$input x $result = $calc<br>";
+        }
+    }
+    ?>
 </main>
-
-    
 </body>
 </html>
