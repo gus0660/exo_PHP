7d868dc6824c3626f0ce720be87bfeff4441e50f
gus0660 <guillaumeroch3@gmail.com>
2024-03-11 16:52:44 +0100

    ok pour exo3 en javascript

--- public/pages/exo3.php
+++ public/pages/exo3.php
@@ -29,7 +29,36 @@ include("../common/footer.php");
     //     echo "Merci de saisir une valeur";
     // }
     ?>
+
 </main>
+<script>
+    const form = document.querySelector('#form');
+
+
+
+    const result = document.querySelector('#result');
+    form.addEventListener('submit', function() {
+        event.preventDefault();
+        let rayon = document.querySelector('#rayon').value;
+        console.log(rayon);
+        let perimetre = document.querySelector('#perimetre').checked;
+        let aire = document.querySelector('#aire').checked;
+        if (rayon > 0) {
+            if (perimetre) {
+                let resultat = rayon * 2 * Math.PI;
+                let line = document.createElement('p');
+                line.innerHTML = " le périmètre du cercle de rayon : " + rayon + "<br> est : " + resultat + "<br>";
+                result.appendChild(line);
+            }
+            if (aire) {
+                let resultat = rayon * rayon * Math.PI;
+                let line = document.createElement('p');
+                line.innerHTML = " l'aire du cercle de rayon : " + rayon + "<br> est : " + resultat + "<br>";
+                result.appendChild(line);
+            }
+        }
+    });
+</script>
 </body>
 
 </html>
