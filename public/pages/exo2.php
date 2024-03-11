06df07c56927a2a7890314d79cc9082550842c61
gus0660 <guillaumeroch3@gmail.com>
2024-03-11 09:12:06 +0100

    préparation de l'exo2.php

--- public/pages/exo2.php
+++ public/pages/exo2.php
@@ -3,10 +3,13 @@ include("../common/header.php");
 include("../common/footer.php");
 ?>
 <main class="text-center">
-    <h2 class="p-4">EXERCICE 2</h2>
-
+    <h2 class="p-4">EXERCICE 2 Affichage d'une pyramide</h2>
+    <form method="post" action="exo2.php">
+        <label for="table">Choisir la hauteur de la pyramide</label>
+        <input type="number" id="table" name="table" required>
+        <input type="submit" value="Afficher">
+        <div id="result"></div>
+    </form>
 </main>
-
-    
 </body>
 </html>
