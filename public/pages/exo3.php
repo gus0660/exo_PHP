5d89dba665f0315a4db3339c9d3aa851ee32af17
gus0660 <guillaumeroch3@gmail.com>
2024-03-11 13:38:38 +0100

    préparation du html de l'exo3

--- public/pages/exo3.php
+++ public/pages/exo3.php
@@ -3,8 +3,17 @@ include("../common/header.php");
 include("../common/footer.php");
 ?>
 <main class="text-center">
-    <h2 class="p-4">EXERCICE 3</h2>
+    <h2 class="p-4">EXERCICE 3 Cercle - Périmètre et aire</h2>
+    <form method="post" action="#" id="form">
+        <label for="rayon">Rayon : </label>
+        <input type="number" id="rayon" name="rayon"><br>
+        <label for="perimetre">Périmètre : </label>
+        <input type="checkbox" id="perimetre" name="perimetre" checked><br>
+        <label for="aire">Aire</label>
+        <input type="checkbox" id="aire" name="aire" checked><br>
+        <input type="submit" value="Envoyer">
 
+    </form>
 </main>
 
     
