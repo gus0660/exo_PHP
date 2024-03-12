28478dfbd6b9ce6b33bceda821598208919ce242
gus0660 <guillaumeroch3@gmail.com>
2024-03-12 12:52:46 +0100

    exo4 préparation du html

--- public/pages/exo4.php
+++ public/pages/exo4.php
@@ -3,10 +3,18 @@ include("../common/header.php");
 include("../common/footer.php");
 ?>
 <main class="text-center">
-    <h2 class="p-4">EXERCICE 4</h2>
-
+    <h2 class="p-4">EXERCICE 4 - Trouver le nombre choisi par l'ordinateur</h2>
+    <form action="#" method="post">
+        <input type="hidden" name="reinit" value="yes">
+        <input class="m-3" type="submit" value="Reinitialiser">
+    </form>
+    <form action="#" method="post">
+        <label for="chiffre">Quel est le chiffre : </label>
+        <input type="number" id="chiffre" name="chiffre">
+        <br>
+        <input class="m-3" type="submit" value="Valider">
+    </form>
+    <h3>Saisir une valeur dans le champ ci-dessus</h3>
 </main>
-
-    
 </body>
 </html>
