6094b780b3c6a02ff58ab36ed4027bd404df5468
gus0660 <guillaumeroch3@gmail.com>
2024-03-12 12:54:52 +0100

    ajustage html exo4

--- public/pages/exo4.php
+++ public/pages/exo4.php
@@ -8,11 +8,10 @@ include("../common/footer.php");
         <input type="hidden" name="reinit" value="yes">
         <input class="m-3" type="submit" value="Reinitialiser">
     </form>
-    <form action="#" method="post">
+    <form class="m-3" action="#" method="post">
         <label for="chiffre">Quel est le chiffre : </label>
         <input type="number" id="chiffre" name="chiffre">
-        <br>
-        <input class="m-3" type="submit" value="Valider">
+        <input type="submit" value="Valider">
     </form>
     <h3>Saisir une valeur dans le champ ci-dessus</h3>
 </main>
