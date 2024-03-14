1133b8aadd1781333c67bbad039388717f01f8e5
gus0660 <guillaumeroch3@gmail.com>
2024-03-14 08:41:56 +0100

    supression commmentaire

--- public/pages/exo4.php
+++ public/pages/exo4.php
@@ -36,7 +36,7 @@ if(!isset($_SESSION['chiffreAleatoire'])) {// si la variable(chiffreAleatoire) n
 }
 
 if (isset($_POST['chiffre'])) {// on va chercher le "post"
-//    $chiffre = $_POST['chiffre']; on sauvegarde le "post" dans la variable(chiffre)
+    $chiffre = $_POST['chiffre'];// on sauvegarde le "post" dans la variable(chiffre)
 //    if ($chiffre < $_SESSION['chiffreAleatoire']) { si le "post" est inférieur
         // echo "Le nombre choisi est TROP PETIT !!";
 //    } else if ($chiffre > $_SESSION['chiffreAleatoire']) { si le "post" est supérieur
