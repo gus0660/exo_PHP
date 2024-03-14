f6ba5e29611e5f9953c244788629f84d9d341b36
gus0660 <guillaumeroch3@gmail.com>
2024-03-14 09:05:56 +0100

    ok pour la base, le formulaire

--- public/pages/exo5.php
+++ public/pages/exo5.php
@@ -4,6 +4,13 @@ include("../common/footer.php");
 ?>
 <main class="text-center">
     <h2 class="p-4">EXERCICE 5 - Calculer une moyenne</h2>
+    <form action="#" method="get">
+        <label for="nb">Combien de notes : </label>
+        <input type="number" name="nb" id="nb">
+        <br>
+        <input type="submit" value="Valider">
+    </form>
+    <h3>Saisir une valeur dans le champ ci-dessus</h3>
 
 </main>
 
