837b25a3c9b262dbfeb6de814f2c554d9909eebf
gus0660 <groch1@hotmail.fr>
2024-03-15 18:03:32 +0100

    mise à jour sur pc msi

--- public/pages/exo5.php
+++ public/pages/exo5.php
@@ -11,35 +11,42 @@ include("../common/footer.php");
     </form>
     <h3>Saisir une valeur dans le champ ci-dessus</h3>
 
-<?php
-// le but : après avoir entré une valeur dans l'input "Combien de notes", et valider, générer le nombre de notes sous forme d'inputs avec un label "note N°1, 2, etc..
-// donc il faut définir la variable nbNotes en allant chercher la valeur du "get" avec $_GET et générer un tableau qui soit un fomulaire avec le nombre de notes et un bouton "calculer" pour calculer la moyenne.
-// ensuite, on peut calculer la moyenne de ces notes et afficher la valeur de la moyenne dans un label "moyenne"
-// pour calculer la moyenne, il faut récupérer les valeurs des notes qu'on aura entré dans le tableau qui à été généré précédemment
-// pour récupérer les valeurs des notes qu'on aura entré dans le tableau qui à été généré précédemment, on peut utiliser la fonction array_sum()
-if(isset($_GET['nb']) && $_GET['nb'] > 0){
-$nbNotes = $_GET['nb'];
-// echo "<h4>$nbNotes notes</h4>";
-    for ($i = 1; $i <= $nbNotes; $i++) {
-        echo "<form action='#' method='get'>";
-        echo "<label class='m-1' for='note'>Note $i : </label>";
-        echo "<input class='m-1' type='number' name='note' id='note'>";
-        echo "<br>";
-        echo "</form>";
-    }
-    echo "<h5 class='p-1'>Veuillez Saisir les notes pour calculer leur moyenne</h5>";
-    echo "<input class='m-1' type='submit' value='Calculer'>";
-}
-if(isset($_GET['note']) && $_GET['note'] > 0){
-    $moyenne = array_sum($_GET['note']) / $nbNotes;
-    echo "<h5 class='p-1'>La moyenne est : $moyenne</h5>";
-}
+    <?php
+    // le but : après avoir entré une valeur dans l'input "Combien de notes", et valider, générer le nombre de notes sous forme d'inputs avec un label "note N°1, 2, etc..
+    // donc il faut définir la variable nbNotes en allant chercher la valeur du "get" avec $_GET et générer un tableau qui soit un fomulaire avec le nombre de notes et un bouton "calculer" pour calculer la moyenne.
+    // ensuite, on peut calculer la moyenne de ces notes et afficher la valeur de la moyenne dans un label "moyenne"
+    // pour calculer la moyenne, il faut récupérer les valeurs des notes qu'on aura entré dans le tableau qui à été généré précédemment
+    // pour récupérer les valeurs des notes qu'on aura entré dans le tableau qui à été généré précédemment, on peut utiliser la fonction array_sum()
+    if (isset($_GET['nb']) && $_GET['nb'] > 0) {
+        $nbNotes = $_GET['nb'];
+        // echo "<h4>$nbNotes notes</h4>";
+        for ($i = 1; $i <= $nbNotes; $i++) {
+            echo "<form action='#' method='post'>";
+            echo "<label class='m-1' for='note'>Note $i : </label>";
+            echo "<input class='m-1' type='number' name='note' id='note'>";
+            echo "<br>";
+            echo "</form>";
+        }
+        echo "<h5 class='p-1'>Veuillez Saisir les notes pour calculer leur moyenne</h5>";
+        echo "<input class='m-1' type='submit' value='Calculer'>";
+        if (isset($_POST['note']) && $_POST['note'] > 0) {
 
-?>
+            $moyenne = array_sum($_POST['note']) / $nbNotes;
+            
+        }
+        echo "<h5 class='p-1'>La moyenne de vos notes est : $moyenne</h5>";
+    }
+
+    // if(isset($_GET['note'])){
+    //     $moyenne = array_sum($_GET['note']) / $nbNotes;
+    //     echo "<h5 class='p-1'>La moyenne des notes est : $moyenne</h5>";
+    // }
+
+    ?>
 
 </main>
 
-    
-</body>
-</html>
 
+</body>
+
+</html>
