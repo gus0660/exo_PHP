97a0732228b13e3c27f45479f549e7287a106824
gus0660 <groch1@hotmail.fr>
2024-03-16 07:56:55 +0100

    ok exo6 completed successfully

--- public/pages/exo6.php
+++ public/pages/exo6.php
@@ -7,20 +7,27 @@ include("../common/footer.php");
     <h2>Selection du personnage</h2>
     <form action="#" method="post">
         <label for="perso">Personnage : </label>
-        <select id="perso" name="perso">
+        <select class="m-2" id="perso" name="perso">
             <option value="homme">Homme</option>
             <option value="femme">Femme</option>
-        </select>
-        <input type="submit" value="Valider">
+        </select><br>
+        <input class="m-2" type="submit" value="Valider">
     </form>
-
+        <?php
+    if (isset($_POST['perso'])) {
+        $perso = $_POST['perso'];
+        if ($perso == "homme") {
+            echo "<img src='./images/player.png' alt='homme' width='100' height='150'>";
+        } else {
+            echo "<img src='./images/playerF.png' alt='femme' width='100' height='150'>";
+        }
+    }
+    ?>
 </main>
 
     
 </body>
 </html>
-<!-- le but : avoir un input qui nous permet de choisir entre homme ou femme, après avoir validé avec le bouton "valider" -->
-<!-- une image en rapport au choix qu'on a fait, apparait -->
-<?php
+<!-- le but : avoir un input qui nous permet de choisir entre homme ou femme,
+après avoir validé avec le bouton "valider", une image en rapport au choix qu'on a fait, apparait -->
 
-?>
