59d12ab3686c71632fdecddd326e5c33df073da0
gus0660 <groch1@hotmail.fr>
2024-03-16 20:25:24 +0100

    ok pour html exo8

--- public/pages/exo8.php
+++ public/pages/exo8.php
@@ -3,8 +3,23 @@ include("../common/header.php");
 include("../common/footer.php");
 ?>
 <main class="text-center">
-    <h2 class="p-4">EXERCICE 8 - Sélection du personnage</h2>
+    <h1 class="p-4">EXERCICE 8 - Sélection du personnage</h1>
+    <form action="#" method="post">
+        <label for="perso">Personnage : </label>
+        <select name="perso" id="perso" onchange="submit()">
+            <option value="p1" selected>Luke</option>
+            <option value="p2">Katy</option>
+            <option value="p3">Marc</option>
+        </select>
+        <br>
+    </form>
+    <h2>Personnage : </h2>
+    <div class="gauche"></div>
+    <div class="gauche"></div>
+    <div class="clearB"></div>
+<?php
 
+?>
 </main>
 
     
