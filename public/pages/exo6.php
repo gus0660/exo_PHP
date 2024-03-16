fc8b66dca0e0bb8d83c6a8ef41f15c26708d1fdd
gus0660 <groch1@hotmail.fr>
2024-03-16 07:40:56 +0100

    formulaire html exo6

--- public/pages/exo6.php
+++ public/pages/exo6.php
@@ -3,7 +3,16 @@ include("../common/header.php");
 include("../common/footer.php");
 ?>
 <main class="text-center">
-    <h2 class="p-4">EXERCICE 6 - Choix du personnage</h2>
+    <h1 class="p-4">EXERCICE 6 - Choix du personnage</h1>
+    <h2>Selection du personnage</h2>
+    <form action="#" method="post">
+        <label for="perso">Personnage : </label>
+        <select id="perso" name="perso">
+            <option value="homme">Homme</option>
+            <option value="femme">Femme</option>
+        </select>
+        <input type="submit" value="Valider">
+    </form>
 
 </main>
 
