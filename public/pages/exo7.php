3ba44bb6d140fc9628374303a4c704603dc6439e
gus0660 <groch1@hotmail.fr>
2024-03-16 19:54:30 +0100

    ok exo7 fait

--- public/pages/exo7.php
+++ public/pages/exo7.php
@@ -5,8 +5,38 @@ include("../common/footer.php");
 <main class="text-center">
     <h2 class="p-4">EXERCICE 7</h2>
 
+    <?php
+    $tableau = array(2, 6, 12, 5, 26, 34, 40, 60);
+    // créer une fonction qui vérifie si un tableau ne contient que des valeurs impaires ou non
+    function is_impair($tableau){
+        foreach ($tableau as $valeur) {
+            if ($valeur % 2 == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // Afficher un message pour l'indiquer à l'utilisateur Résultat : Le tableau contient des valeurs impaires
+    if (is_impair($tableau)) {
+        echo "Le tableau ne contient pas des valeurs impaires";
+    } else {
+        echo "Le tableau contient des valeurs impaires";
+    }
+    ?>
+
+
+
 </main>
 
-    
+
 </body>
+
 </html>
+<!-- Etape 1
+Réaliser un tableau contenant les valeurs : 2,6,12,5,26,34,40,60
+Etape 2
+Réaliser une fonction qui vérifie si un tableau ne contient que des valeurs paires ou non
+Etape 3
+Afficher un message pour l'indiquer à l'utilisateur
+Résultat :
+Le tableau contient des valeurs impaires -->
