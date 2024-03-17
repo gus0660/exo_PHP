544c6d414ffe067d73c8fffded05b58c3957e7da
gus0660 <groch1@hotmail.fr>
2024-03-17 16:40:45 +0100

    premier php dans exo10

--- public/pages/exo10.php
+++ public/pages/exo10.php
@@ -6,7 +6,34 @@ include("../common/footer.php");
 <h1 class="p-4">EXERCICE 10 - Sélection du personnage</h1>
     <h2>Personnage : </h2>
     
-    
+<?php
+$persos = [
+    "p1" => [
+        "Nom" => "Luke",
+        "Age" => 27,
+        "Sexe" => "Homme",
+        "Force" => "5",
+        "Agilité" => "4",
+        "Image" => "./images/player.png"
+    ],
+    "p2" => [
+        "Nom" => "Katy",
+        "Age" => 22,
+        "Sexe" => "Femme",
+        "Force" => "3",
+        "Agilité" => "6",
+        "Image" => "./images/playerF.png"
+    ],
+    "p3" => [
+        "Nom" => "Marc",
+        "Age" => 33,
+        "Sexe" => "Homme",
+        "Force" => "7",
+        "Agilité" => "2",
+        "Image" => "./images/playerM.png"
+    ]
+];
+?>
 </main>
 
     
